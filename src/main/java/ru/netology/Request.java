3c92427192f61adc7eebddd6dff45c72fefd6933
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-09-15 10:54:37 +0700

    change Request

--- src/main/java/ru/netology/Request.java
+++ src/main/java/ru/netology/Request.java
@@ -1,40 +1,74 @@
 package ru.netology;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class Request {
 
-    private List<String> headers = new ArrayList<>();
-    private String body;
-    private BufferedReader in;
-    private String request;
-    private String path;
+    private final String method;
+    private final String path;
+    private final List<String> headers;
+    private final String body;
 
-    public Request(String request, String path, Socket client) {
-        this.request = request;
+    public Request(String method, String path, List<String> headers, String body) {
+        this.method = method;
         this.path = path;
-        try {
-            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        } catch (IOException e) {
-            System.err.println(e);
-        }
+        this.headers = headers;
+        this.body = body;
     }
 
-    public Object request() {
-        boolean check = false;
-        for (String header : headers) {
-            if (request.equals(header)) {
-                check = true;
-            }
-        }
-        if (check) {
-            return body;
-        }
-        return in;
+//    public void parsingRequestLine(String requestLine) throws IOException {
+//
+//        final var filePath = Path.of(".", "public", path);
+//        final var mimeType = Files.probeContentType(filePath);
+//
+//
+//
+//        if (path.equals("/classic.html")) {
+//            final var template = Files.readString(filePath);
+//            final var content = template.replace(
+//                    "{time}",
+//                    LocalDateTime.now().toString()
+//            ).getBytes();
+//            out.write((
+//                    "HTTP/1.1 200 OK\r\n" +
+//                            "Content-Type: " + mimeType + "\r\n" +
+//                            "Content-Length: " + content.length + "\r\n" +
+//                            "Connection: close\r\n" +
+//                            "\r\n"
+//            ).getBytes());
+//            out.write(content);
+//            out.flush();
+//        }
+//
+//        final var length = Files.size(filePath);
+//        out.write((
+//                "HTTP/1.1 200 OK\r\n" +
+//                        "Content-Type: " + mimeType + "\r\n" +
+//                        "Content-Length: " + length + "\r\n" +
+//                        "Connection: close\r\n" +
+//                        "\r\n"
+//        ).getBytes());
+//        Files.copy(filePath, out);
+//        out.flush();
+//    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public List<String> getHeaders() {
+        return headers;
     }
 }
