528b9ef8619844e9a8348c748d1b7e8eb730dc67
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-09-15 10:55:10 +0700

    change Main & create Handler interface

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -5,23 +5,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
-        final var validPaths = List.of("/index.html", "/spring.svg",
+
+        final List<String> validPaths = List.of("/index.html", "/spring.svg",
                 "/spring.png", "/resources.html", "/styles.css", "/app.js",
                 "/links.html", "/forms.html", "/classic.html",
                 "/events.html", "/events.js");
+        final List<String> validMethods = List.of("GET", "POST", "PUT", "DELETE");
+
         final var connections = Executors.newFixedThreadPool(64);
         final var clients = new ConcurrentHashMap<Integer, Socket>();
+        final var notFound404 =
+                "HTTP/1.1 404 Not Found\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n";
 
         final var server = new Server();
+        server.addHandler("GET", "/messages", (request, responseStream) -> {
+            // code
+        });
+        server.addHandler("POST", "/message", (request, responseStream) -> {
+            // code
+        });
         server.start(9999);
         System.out.println("Server is up.");
 
@@ -36,56 +48,53 @@ public class Main {
                                         client.getInputStream()));
                              var out = new BufferedOutputStream(
                                      client.getOutputStream())) {
-                            final var requestLine = in.readLine();
+
+                            List<String> lines = new ArrayList<>();
+                            String line;
+                            while ((line = in.readLine()) != null) {
+                                lines.add(line);
+                            }
+
+                            final var requestLine = lines.getFirst();
                             final var parts = requestLine.split(" ");
 
                             if (parts.length != 3) {
+                                out.write(notFound404.getBytes());
+                                out.flush();
                                 continue;
                             }
 
-                            final var path = parts[1];
+                            String method = parts[0];
+                            if (!validMethods.contains(method)) {
+                                out.write(notFound404.getBytes());
+                                out.flush();
+                                continue;
+                            }
+
+                            String path = parts[1];
                             if (!validPaths.contains(path)) {
-                                out.write((
-                                        "HTTP/1.1 404 Not Found\r\n" +
-                                                "Content-Length: 0\r\n" +
-                                                "Connection: close\r\n" +
-                                                "\r\n"
-                                ).getBytes());
+                                out.write(notFound404.getBytes());
                                 out.flush();
                                 continue;
                             }
 
-                            final var filePath = Path.of(".", "public", path);
-                            final var mimeType = Files.probeContentType(filePath);
-
-                            if (path.equals("/classic.html")) {
-                                final var template = Files.readString(filePath);
-                                final var content = template.replace(
-                                        "{time}",
-                                        LocalDateTime.now().toString()
-                                ).getBytes();
-                                out.write((
-                                        "HTTP/1.1 200 OK\r\n" +
-                                                "Content-Type: " + mimeType + "\r\n" +
-                                                "Content-Length: " + content.length + "\r\n" +
-                                                "Connection: close\r\n" +
-                                                "\r\n"
-                                ).getBytes());
-                                out.write(content);
-                                out.flush();
-                                continue;
+                            List<String> headers = new ArrayList<>();
+                            for (int i = 2; i < lines.size() &&
+                                    !lines.get(i).isEmpty(); i++) {
+                                headers.add(lines.get(i));
                             }
 
-                            final var length = Files.size(filePath);
-                            out.write((
-                                    "HTTP/1.1 200 OK\r\n" +
-                                            "Content-Type: " + mimeType + "\r\n" +
-                                            "Content-Length: " + length + "\r\n" +
-                                            "Connection: close\r\n" +
-                                            "\r\n"
-                            ).getBytes());
-                            Files.copy(filePath, out);
-                            out.flush();
+                            StringBuilder bodyBuilder = new StringBuilder();
+                            if (lines.get(headers.size()).isEmpty()) {
+                                for (int i = headers.size() + 1; i < lines.size(); i++) {
+                                    bodyBuilder.append(lines.get(i));
+                                }
+                            }
+
+                            String body = bodyBuilder.toString();
+
+                            Request request = new Request(method, path, headers, body);
+
                         } catch (IOException e) {
                             System.err.println(e);
                         }
