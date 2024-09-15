e66e478c2893facd34e755be19ef02ba2cbbf66f
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-09-15 10:55:28 +0700

    change Server

--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -3,12 +3,14 @@ package ru.netology;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.logging.Handler;
-
+import java.util.concurrent.ConcurrentHashMap;
 public class Server {
 
     private ServerSocket server = null;
     private Socket client = null;
+    private final ConcurrentHashMap<String,
+            ConcurrentHashMap<String, Handler>> handlers =
+            new ConcurrentHashMap<>();
 
     public void start(int port) {
         try {
@@ -32,7 +34,20 @@ public class Server {
         return client;
     }
 
-    public void addHandler(String request, String path, Handler handler) {
+    public void addHandler(String method, String path, Handler handler) {
 
+//        ConcurrentHashMap<String, Handler> paths = handlers.computeIfAbsent(
+//                method, k -> new ConcurrentHashMap<>());
+//        paths.put(path, handler);
+
+
+        if (!handlers.containsKey(method)) {
+            var paths = new ConcurrentHashMap<String, Handler>();
+            paths.put(path, handler);
+            handlers.put(method, paths);
+        } else {
+            var paths = handlers.get(method);
+            paths.put(path, handler);
+        }
     }
 }
