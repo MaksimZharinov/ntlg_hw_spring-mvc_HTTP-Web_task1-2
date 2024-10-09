2be09e1114537703121e6252aa72455f6675facd
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-10-10 02:37:32 +0700

    change Request to get POST params

--- src/main/java/ru/netology/Request.java
+++ src/main/java/ru/netology/Request.java
@@ -4,17 +4,19 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Request {
 
+    private final String typeMultipart = "multipart/form-data";
     private final String method;
     private final String path;
     private final List<String> headers;
     private final String body;
     private final List<NameValuePair> queryParams;
-
+    private final List<NameValuePair> postParams;
 
     public Request(String method, String path, List<String> headers, String body) {
         this.method = method;
@@ -22,6 +24,14 @@ public class Request {
         this.headers = headers;
         this.body = body;
         queryParams = URLEncodedUtils.parse(path, StandardCharsets.UTF_8);
+        var contentType = headers.stream()
+                .filter(h -> h.contains("Content-Type"))
+                .collect(Collectors.joining());
+        if (contentType.contains("application/x-www-form-urlencoded")) {
+            postParams = URLEncodedUtils.parse(body, StandardCharsets.UTF_8);
+        } else {
+            postParams = new ArrayList<>();
+        }
     }
 
     public String getMethod() {
@@ -46,6 +56,16 @@ public class Request {
 
     public List<NameValuePair> getQueryParam(String name) {
         return queryParams.stream()
+                .filter(p -> p.getName().equals(name))
+                .collect(Collectors.toList());
+    }
+
+    public List<NameValuePair> getPostParams() {
+        return postParams;
+    }
+
+    public List<NameValuePair> getPostParam(String name) {
+        return postParams.stream()
                 .filter(p -> p.getName().equals(name))
                 .collect(Collectors.toList());
     }
